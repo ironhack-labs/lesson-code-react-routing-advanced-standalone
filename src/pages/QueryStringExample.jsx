913296b3c978990a7d8bc02bb720bad9f75bc604
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2023-05-30 14:21:32 +0200

    refactor: QueryStringExample component - remove effect

--- src/pages/QueryStringExample.jsx
+++ src/pages/QueryStringExample.jsx
@@ -1,27 +1,23 @@
-import { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams } from "react-router-dom";
 
-function QueryStringExample(props) {
+function QueryStringExample() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  // Get the values from the URL query strings 
-  // Example: http://localhost:3000/example?place=Miami&destType=Apartment
+  // Get the values from the URL query strings
+  // Example 1: http://localhost:5173/example?place=Miami&destType=Apartment
+  // Example 2: http://localhost:5173/example?place=Paris&destType=House
   const place = searchParams.get("place");
   const destType = searchParams.get("destType");
-  
-  useEffect(() => {
-    console.log('place', place);
-    console.log('destType', destType);
-  }, []);
+
+  console.log("place", place);
+  console.log("destType", destType);
 
   return (
     <div>
       <h2>Query String Example</h2>
-      <p>
-        Open the console to see the logged query string values
-      </p>
+      <p>Open the console to see the logged query string values</p>
     </div>
-  )
+  );
 }
 
 export default QueryStringExample;
